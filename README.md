fd1d00478d97852ce59012b23d4387b377470dba
Valeria Spektor <vlapin84@gmail.com>
2024-01-17 20:21:09 +0200

    added js file task score

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hw_2.1.3
+Student: Irina Lapina
+Scores for Introduction: 80
+Scores for Git: 92
+Scores for JavaScript: 68
+Average Score: 80
---
+++ task_score.js
@@ -0,0 +1,18 @@
+// Student scores by modules
+let introductionScore = 80;
+let gitScore = 92;
+let jsScore = 68;
+
+// Calculating the average score
+let averageScore = (introductionScore + gitScore + jsScore) / 3;
+
+// Student information
+let studentName = "Irina";
+let studentLastName = "Lapina";
+
+// Console output with template strings
+console.log(`Student: ${studentName} ${studentLastName}`);
+console.log(`Scores for Introduction: ${introductionScore}`);
+console.log(`Scores for Git: ${gitScore}`);
+console.log(`Scores for JavaScript: ${jsScore}`);
+console.log(`Average Score: ${averageScore}`);
